73f78240881b4a7ad5eda90ad68efd34e4e02341
Katelyn Hylbom <khylbom@uci.edu>
2018-11-24 13:09:49 -0800

    cleaned sample data and consolidated SleepService

--- sleeptracker/src/app/pages/home/home.page.ts
+++ sleeptracker/src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SleepService } from '../../services/sleep.service';
-import { SampleDataService } from '../../services/sample-data.service';
+import { SleepData } from '../../data/sleep-data';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +9,21 @@ import { SampleDataService } from '../../services/sample-data.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private sleepService: SleepService) { }
+  constructor(private sleepService: SleepService) {
+    this.getSampleData();
+  }
 
   ngOnInit() {
-    this.sleepService.addSampleData();
-    console.log(this.allSleepData);
+    console.log('HomePage has allSleepData: ' + this.allSleepData);
   }
 
   /* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
   get allSleepData() {
     return SleepService.AllSleepData;
+  }
+
+  async getSampleData() {
+    return await this.sleepService.getSampleData();
   }
 
 }
--- sleeptracker/src/app/services/sleep.service.ts
+++ sleeptracker/src/app/services/sleep.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
-import { SampleDataService } from './sample-data.service';
+import { HttpClient } from '@angular/common/http';
+
+const sample_overnight_data_url = 'assets/json/sample-overnight-data.min.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SleepService {
-  private static LoadDefaultData = true;
+  private static LoadSampleData = true;
+  private static LoadDefaultData = false;
   public static AllSleepData: SleepData[] = [];
   public static AllOvernightData: OvernightSleepData[] = [];
   public static AllSleepinessData: StanfordSleepinessData[] = [];
 
-  constructor(private sampleDataService: SampleDataService) {
-   if (SleepService.LoadDefaultData) {
+  constructor(private http: HttpClient) {
+    if (SleepService.LoadDefaultData) {
       this.addDefaultData();
       SleepService.LoadDefaultData = false;
     }
@@ -24,10 +27,6 @@ export class SleepService {
     this.logOvernightData(new OvernightSleepData(new Date('November 12, 2018 01:03:00'), new Date('November 12, 2018 09:25:00')));
     this.logSleepinessData(new StanfordSleepinessData(4, new Date('November 12, 2018 14:38:00')));
     this.logOvernightData(new OvernightSleepData(new Date('November 12, 2018 23:11:00'), new Date('November 13, 2018 08:03:00')));
-  }
-
-  async addSampleData() {
-    return await this.sampleDataService.logSampleData(this);
   }
 
   public logOvernightData(data) {
@@ -40,4 +39,33 @@ export class SleepService {
     SleepService.AllSleepinessData.push(sleepData);
   }
 
+  private addSampleData() {
+    return new Promise<void>(resolve => {
+      try {
+        this.http.get(sample_overnight_data_url)
+            .toPromise()
+            .then(data => data as string[])
+            .then(items => {
+              items.forEach(element => {
+                const sleepStart = new Date(element['sleepStart']);
+                const sleepEnd = new Date(element['sleepEnd']);
+                this.logOvernightData(new OvernightSleepData(sleepStart, sleepEnd));
+              });
+            });
+      } catch (error) {
+        console.log(error);
+      }
+      SleepService.LoadSampleData = false;
+      resolve();
+    });
+  }
+
+  public async getSampleData() {
+    if (SleepService.LoadSampleData) {
+      await this.addSampleData().then(() => {
+        return SleepService.AllSleepData;
+      });
+    }
+    return SleepService.AllSleepData;
+  }
 }
