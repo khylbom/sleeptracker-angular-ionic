97ee09cdb4f8165d25d7557ff996010ddf8a7730
Katelyn Hylbom <khylbom@uci.edu>
2018-11-30 17:13:42 -0800

    Clean up code for history page

--- sleeptracker/src/app/pages/history/history.page.ts
+++ sleeptracker/src/app/pages/history/history.page.ts
@@ -8,12 +8,10 @@ import { SleepData } from 'src/app/data/sleep-data';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-  private static AllSleepDataSorted: SleepData[] = [];
 
   constructor(private sleepService: SleepService) { }
 
   ngOnInit() {
-    // HistoryPage.AllSleepDataSorted = SleepService.AllSleepData.slice().reverse();
   }
 
   // use slice() to make a copy of the array and leave original unmodified
